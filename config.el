995cfba760473eeadb3e12a724d19f014acee846
zhangruobin001 <zhangruobin001@ke.com>
2021-12-24 15:00:07 +0800

    config commit

--- config.el
+++ config.el
@@ -1,19 +1,22 @@
 (setq display-line-numbers-type t)
 (setq fancy-splash-image "/Users/zhangruobin/.doom.d/static/mc.png")
 (with-eval-after-load 'evil-maps
-    (define-key evil-normal-state-map (kbd ",w") 'evil-write))
+  (define-key evil-normal-state-map (kbd ",w") 'evil-write))
 
-(setq doom-theme 'vscode-dark-plus)
+(exec-path-from-shell-copy-env "GOPROXY")
+(exec-path-from-shell-copy-env "GOROOT")
 
-;; Font
-(setq doom-font (font-spec :family "Cascadia Code PL" :size 12))
+    (setq doom-theme 'vscode-dark-plus)
+
+    ;; Font
+    (setq doom-font (font-spec :family "Cascadia Code PL" :size 13))
 
 (setq url-proxy-services '(("http" . "127.0.0.1:7890")
                            ("https" . "127.0.0.1:7890")))
 
-;; mirror setting
-(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-                        ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+    ;; mirror setting
+    (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                            ("melpa" . "http://elpa.emacs-china.org/melpa/")))
 
 (map! :leader
       :desc "Code Outline"
@@ -41,19 +44,23 @@
 
 (setq lsp-file-watch-threshold 3000)
 
-;;Add this for bug of org mode
-(defun native-comp-available-p () nil)
-(setq org-directory "~/org/")
+    ;;Add this for bug of org mode
+    (defun native-comp-available-p () nil)
+    (setq org-directory "~/org/")
 
-;; Java config
-;; (setenv "JAVA_HOME" "/Library/Java/JavaVirtualMachines/jdk-15.0.2.jdk/Contents/Home")
-(setq lsp-java-vmargs '("-Xmx2G" "-Xms1G" "-XX:+UseG1GC" "-XX:+UseStringDeduplication" "-javaagent:/Users/zhangruobin/.emacs.d/.local/etc/lsp/eclipse.jdt.ls/lombok.jar"))
-(setq lsp-java-configuration-runtimes '[(:name "JavaSE-1.8"
-                                        :path "/Library/Java/JavaVirtualMachines/jdk1.8.0_291.jdk/Contents/Home")
-                                        (:name "JavaSE-15"
-                                        :path "/usr/local/Cellar/openjdk/15.0.1/"
-                                        :default t)])
+    ;; Java config
+    ;; (setenv "JAVA_HOME" "/Library/Java/JavaVirtualMachines/jdk-15.0.2.jdk/Contents/Home")
+    (setq lsp-java-vmargs '("-Xmx2G" "-Xms1G" "-XX:+UseG1GC" "-XX:+UseStringDeduplication" "-javaagent:/Users/zhangruobin/.emacs.d/.local/etc/lsp/eclipse.jdt.ls/lombok.jar"))
+    (setq lsp-java-configuration-runtimes '[(:name "JavaSE-1.8"
+                                            :path "/Library/Java/JavaVirtualMachines/jdk1.8.0_291.jdk/Contents/Home")
+                                            (:name "JavaSE-15"
+                                            :path "/usr/local/Cellar/openjdk/15.0.1/"
+                                            :default t)])
 
 (setq dap-auto-configure-features '(sessions locals controls tooltip))
 
-(setq-default evil-escape-key-sequence "jj")
+    (setq-default evil-escape-key-sequence "jj")
+
+;;(require 'eaf)
+;;(require 'eaf-browser)
+;;(setq eaf-python-command "/usr/local/bin/python3")
