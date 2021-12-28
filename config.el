9a34aaa6dfe5465dc2f61f6f97f2c704bd22ec6f
zhangruobin001 <zhangruobin001@ke.com>
2021-12-28 11:54:20 +0800

    leetcode submit

--- config.el
+++ config.el
@@ -6,17 +6,51 @@
 (exec-path-from-shell-copy-env "GOPROXY")
 (exec-path-from-shell-copy-env "GOROOT")
 
-    (setq doom-theme 'vscode-dark-plus)
+(setq doom-theme 'adwaita)
 
-    ;; Font
-    (setq doom-font (font-spec :family "Cascadia Code PL" :size 13))
+;; Font
+(setq doom-font (font-spec :family "Cascadia Code PL" :size 13))
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+
+(when (window-system)
+  (set-frame-font "Fira Code"))
+(let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
+               (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
+               (36 . ".\\(?:>\\)")
+               (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
+               (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
+               (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
+               (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
+               (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
+               (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
+               (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
+               (48 . ".\\(?:x[a-zA-Z]\\)")
+               (58 . ".\\(?:::\\|[:=]\\)")
+               (59 . ".\\(?:;;\\|;\\)")
+               (60 . ".\\(?:\\(?:!--\\)\\|\\(?:~~\\|->\\|\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[*$+~/<=>|-]\\)")
+               (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
+               (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
+               (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
+               (91 . ".\\(?:]\\)")
+               (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
+               (94 . ".\\(?:=\\)")
+               (119 . ".\\(?:ww\\)")
+               (123 . ".\\(?:-\\)")
+               (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
+               (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)")
+               )
+             ))
+  (dolist (char-regexp alist)
+    (set-char-table-range composition-function-table (car char-regexp)
+                          `([,(cdr char-regexp) 0 font-shape-gstring]))))
 
 (setq url-proxy-services '(("http" . "127.0.0.1:7890")
                            ("https" . "127.0.0.1:7890")))
 
-    ;; mirror setting
-    (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-                            ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+;; mirror setting
+(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                        ("melpa" . "http://elpa.emacs-china.org/melpa/")))
 
 (map! :leader
       :desc "Code Outline"
@@ -44,23 +78,36 @@
 
 (setq lsp-file-watch-threshold 3000)
 
-    ;;Add this for bug of org mode
-    (defun native-comp-available-p () nil)
-    (setq org-directory "~/org/")
+;;Add this for bug of org mode
+(defun native-comp-available-p () nil)
+(setq org-directory "~/org/")
 
-    ;; Java config
-    ;; (setenv "JAVA_HOME" "/Library/Java/JavaVirtualMachines/jdk-15.0.2.jdk/Contents/Home")
-    (setq lsp-java-vmargs '("-Xmx2G" "-Xms1G" "-XX:+UseG1GC" "-XX:+UseStringDeduplication" "-javaagent:/Users/zhangruobin/.emacs.d/.local/etc/lsp/eclipse.jdt.ls/lombok.jar"))
-    (setq lsp-java-configuration-runtimes '[(:name "JavaSE-1.8"
-                                            :path "/Library/Java/JavaVirtualMachines/jdk1.8.0_291.jdk/Contents/Home")
-                                            (:name "JavaSE-15"
-                                            :path "/usr/local/Cellar/openjdk/15.0.1/"
-                                            :default t)])
+;; Java config
+;; (setenv "JAVA_HOME" "/Library/Java/JavaVirtualMachines/jdk-15.0.2.jdk/Contents/Home")
+(setq lsp-java-vmargs '("-Xmx2G" "-Xms1G" "-XX:+UseG1GC" "-XX:+UseStringDeduplication" "-javaagent:/Users/zhangruobin/.emacs.d/.local/etc/lsp/eclipse.jdt.ls/lombok.jar"))
+(setq lsp-java-configuration-runtimes '[(:name "JavaSE-1.8"
+                                        :path "/Library/Java/JavaVirtualMachines/jdk1.8.0_291.jdk/Contents/Home")
+                                        (:name "JavaSE-15"
+                                        :path "/usr/local/Cellar/openjdk/15.0.1/"
+                                        :default t)])
 
 (setq dap-auto-configure-features '(sessions locals controls tooltip))
 
-    (setq-default evil-escape-key-sequence "jj")
+(setq-default evil-escape-key-sequence "jj")
 
-;;(require 'eaf)
-;;(require 'eaf-browser)
-;;(setq eaf-python-command "/usr/local/bin/python3")
+
+
+(defvar leetcode-l "golang")
+(setq leetcode-save-solutions 't)
+(setq leetcode-prefer-language leetcode-l)
+(setq leetcode-directory (concat "~/leetcode/" leetcode-l))
+
+(map! :leader
+      :desc "leetcode"
+      "l t" #'leetcode)
+(map! :leader
+      :desc "leetcode-test"
+      "l T" #'leetcode-try)
+(map! :leader
+      :desc "leetcode-submit"
+      "l s" #'leetcode-submit)
--- custom.el
+++ custom.el
@@ -4,7 +4,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default)))
+   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
+ '(package-selected-packages '(ssh-deploy))
+ '(safe-local-variable-values
+   '((ssh-deploy-async . 1)
+     (ssh-deploy-async . 0)
+     (ssh-deploy-verbose . 1)
+     (ssh-deploy-debug . 1)
+     (ssh-deploy-on-explicit-save . t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
